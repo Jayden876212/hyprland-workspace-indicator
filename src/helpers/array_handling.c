7409e731466da07e04095a2bd28ffa9928e5857b
tdljayden <yt8xne0w@duck.com>
2023-08-24 23:18:07 +0100

    Added comments to create_activeworkspace_array()

--- src/helpers/array_handling.c
+++ src/helpers/array_handling.c
@@ -14,7 +14,7 @@ int create_workspace_array(HyprlandData * hyprland_data) {
     // Initialise pointer to workspace_array to improve code readability (reduces width).
     uint16_t * workspace_array = hyprland_data->workspace_array;
 
-    // Loop through monitors
+    // Loop through monitors.
     for (int i = 0; i < hyprland_data->monitors_length; ++i) {
         // Set each position (the ith monitor in this loop) in the workspace array to the int 0 to 
         // clear it.
@@ -30,7 +30,7 @@ int create_workspace_array(HyprlandData * hyprland_data) {
         cJSON * monitor_name_obj = cJSON_GetObjectItem(ith_monitor, "name");
         char * monitor_name = monitor_name_obj->valuestring;
 
-        // Loop through the used workspaces
+        // Loop through the used workspaces.
         for (int v = 0; v < hyprland_data->workspaces_length; ++v) {
             // Perform steps to grab information about each monitor (the vth monitor in this loop).
             cJSON * vth_workspace = cJSON_GetArrayItem(hyprland_data->workspaces, v);
@@ -68,21 +68,26 @@ int create_workspace_array(HyprlandData * hyprland_data) {
 }
 
 int create_activeworkspace_array(HyprlandData * hyprland_data) {
+    // Initialise pointers to improve code readability (reduces width).
     uint16_t * activeworkspace_array = hyprland_data->activeworkspace_array;
 
+    // Perform steps needed to get the monitor output name (for comparison) and the id of the
+    // workspace ID to show the user using an array (ID is position in the array).
     cJSON * active_monitor_obj = cJSON_GetObjectItem(hyprland_data->activeworkspace, "monitor");
     char * active_monitor = active_monitor_obj->valuestring;
-
     cJSON * workspace_id_obj = cJSON_GetObjectItem(hyprland_data->activeworkspace, "id");
     int workspace_id = workspace_id_obj->valueint;
 
+    // Loop through monitors.
     for (int i = 0; i < hyprland_data->monitors_length; ++i) {
         activeworkspace_array[i] = 0b0000000000000000; 
 
+        // Parse the JSON to grab the monitor name.
         cJSON * ith_element = cJSON_GetArrayItem(hyprland_data->monitors, i);
         cJSON * monitor_name_obj = cJSON_GetObjectItem(ith_element, "name");
         char * monitor_name = monitor_name_obj->valuestring;
 
+        // See previous comments on the other function
         if (strcmp(active_monitor, monitor_name) == 0) {
             int target_workspace_id = (workspace_id != 10) ? workspace_id : 0;
             activeworkspace_array[i] = turn_on_bit_in_array(
