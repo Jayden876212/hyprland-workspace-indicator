4a9b2ccaf7b386f745b5a0c2087039f96e501449
tdljayden <yt8xne0w@duck.com>
2023-08-31 19:38:18 +0100

    Add helpful comments to the json handling explaining the process

--- src/helpers/json_handling.c
+++ src/helpers/json_handling.c
@@ -18,18 +18,28 @@
 #include "utils/hyprland_socket_handling.h"
 #include "utils/bit_handling.h"
 
+// Void function is used to reduce repeated code as both workspaces_array and activeworkspace_array
+// have a similar printing method within the array.
 void print_array_json_formatted(uint16_t bit_array) {
     for (int n = 0; n < WORKSPACES_AMOUNT; ++n) {
+        // Access a bit among the specific 16 bits at the nth position.
         bool accessed_bit = access_bit_array(bit_array, n);
+        // Check what the accessed bit evaluates to and print true or false. This is so the user of
+        // the program can use these values to hide or unhide a numbered workspace indicator by
+        // looking up the number in the array and checking if it is true or false.
         accessed_bit ? printf("true") : printf("false");
 
-        if (n != WORKSPACES_AMOUNT - 1)
-            printf(",");
+        if (n != WORKSPACES_AMOUNT - 1) // Checks if we are at the end of the bool array. We would
+                                        // not want to print a comma at the end because that is
+                                        // invalid json.
+            printf(","); // Print a comma to seperate values so it is valid json to be parsed.
     }
 }
 
+// We are using a function to print json manually rather than using cJSON_Print because this will
+// create unnecessary complexity with the cJSON object. This should result in better performance.
 int print_workspaces_json_array(HyprlandData * hyprland_data) {
-    printf("[");
+    printf("["); // Open the json.
     for (int i = 0; i < hyprland_data->monitors_length; ++i) {
         printf("{\"workspaces\":[");
         print_array_json_formatted(hyprland_data->workspace_array[i]);
@@ -41,12 +51,16 @@ int print_workspaces_json_array(HyprlandData * hyprland_data) {
             printf(",");
         
     }
-    printf("]\n");
-    fflush(stdout);
+    printf("]\n"); // Close the json.
+    fflush(stdout); // We flush stdout because we want the workspace data to be immediately sent to
+                    // the receiving program. This will prevent undefined behaviour resulting from a
+                    // buffered output.
 
     return 0;
 }
 
+// The data that we need about hyprland is pulled using a data structure that is passed to multiple
+// functions.
 int initialise_and_print_workspace_info_as_json() {
     HyprlandData * hyprland_data = initialise_hyprland_data_structure();
 
