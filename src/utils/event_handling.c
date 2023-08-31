582253e3dcae715511540a0a0dd7d7a6e24cd221
tdljayden <yt8xne0w@duck.com>
2023-08-31 17:05:20 +0100

    Add comments explaining handling of events

--- src/utils/event_handling.c
+++ src/utils/event_handling.c
@@ -10,13 +10,19 @@
 #include "data/data_structures.h"
 
 int poll_for_socket_events(SocketData * events_data, void (*event_processor)(), int (*function_executed)()) {
+    // Poll the server for events that we can check for.
     if (poll(events_data->poll_descriptor, 1, -1) == -1) {
         perror("poll");
         return -1;
     }
+
+    // Check if the server has data ready to be read by comparing the constant POLLIN using AND.
     if (events_data->poll_descriptor->revents & POLLIN) {
+        // Initialise pointers to simplify the reading of the code.
         int * file_descriptor = &(events_data->poll_descriptor->fd);
         char * data_received = events_data->data_received;
+
+        // Receive data from the server.
         ssize_t bytes_received = recv(*file_descriptor, data_received, MAX_BUFFER_SIZE + 1, 0);
         if (bytes_received == -1) {
             perror("recv");
@@ -27,6 +33,10 @@ int poll_for_socket_events(SocketData * events_data, void (*event_processor)(), 
         }
 
         events_data->data_received[bytes_received] = '\0';
+
+        // Process the events using a user-specified event processor so we can check what events we
+        // want to respond to. The response is carrying out the user-specified function_executed
+        // function.
         event_processor(events_data, function_executed);
     }
 
@@ -34,8 +44,11 @@ int poll_for_socket_events(SocketData * events_data, void (*event_processor)(), 
 }
 
 void handle_workspace_socket_events(SocketData * events_data, int (*function_executed)()) {
+    // Initialise pointer for better readability.
     char * data_received = events_data->data_received;
 
+    // Check if the user switches workspace or monitor so we can respond to it with the
+    // user-specified function.
     if (strstr(data_received, EVENT_WORKSPACE_CHANGED) ||
         strstr(data_received, EVENT_MONITOR_CHANGED)) {
         function_executed();
