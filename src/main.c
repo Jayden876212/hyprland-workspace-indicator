e9bc6262b69f5f6d001f9c519f58a4b06de391be
Jayden876212 <dxmqkgax@duck.com>
2025-06-30 16:27:13 +0100

    fix(signals): remove redundant signal handling
    Signed-off-by: Jayden876212 <dxmqkgax@duck.com>

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <signal.h>
-
 #include <cjson/cJSON.h>
 
 // include/data
@@ -17,40 +15,24 @@
 #include "utils/event_handling.h"
 #include "utils/hyprland_socket_handling.h"
 
-// Initialise global variable so it can be cleaned up with a signal handler.
-SocketData *events_data_global = NULL;
-// The use of a global variable might seem like a strange choice here, but this is because
-// signal handlers don't accept any other arguments other than the signum. We set the global
-// variable to point to a local variable which is passed amongst multiple functions in
-// event_handling.c and hyprland_socket_handling.c, which is done to allow reusability of the code
-// without the use of a global variable.
-
-// Set up a function for the signal handler so we can cleanup memory and print a message when the
-// user of the program closes the process.
-static void sig_int_handler(int signum) {
-    delete_socket_data_structure(events_data_global);
-    fprintf(stderr, "Exiting due to interrupt. (signal = %d)\n", signum);
-    exit(signum);
-}
 
 int main() {
-    // Set up the signal handler using our signal handler function so we can use defined behaviour
-    // when the user closes the program.
-    signal(SIGINT, sig_int_handler);
+    SocketData *events_data = NULL;
+
+    // We do not need to have a signal handler because the operating system handles closing file
+    // descriptors when the user closes the program.
+    // (See https://stackoverflow.com/questions/9022495/freeing-memory-inside-a-signal-handler)
 
     // Set up the data structure for hyprland's events so we can print the workspace information
     // only when it's needed. This will reduce performance overhead as we won't be constantly
     // printing the workspace information in a loop, rather we will be checking for when the
     // information actually changes.
-    SocketData *events_data = initialise_socket_data_structure();
+    events_data = initialise_socket_data_structure();
     if (events_data == NULL) {
         fprintf(stderr, "Error: Failed to allocate socket data structure. Exiting.\n");
         exit(EXIT_FAILURE); // We exit the program because without a connection to a socket, it is
                             // essentially useless.
     }
-    // Set the global variable to the events_data struct so we may use it in our cleanup after the
-    // program is closed.
-    events_data_global = events_data;
 
     // Set up the hyprland socket so it can be used to poll for events.
     int hyprland_socket_creation_result = set_up_hyprland_socket(SOCKET2, events_data);
