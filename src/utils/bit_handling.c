0236af284a0ccad01a894716b74524fa699ca8c3
Jayden876212 <dxmqkgax@duck.com>
2025-07-04 22:25:58 +0100

    fix(bit_handling): replace magic numbers
    Signed-off-by: Jayden876212 <dxmqkgax@duck.com>

--- src/utils/bit_handling.c
+++ src/utils/bit_handling.c
@@ -15,7 +15,7 @@
 int print_binary(uint16_t integer) { // This is a test function for debugging only.
     printf("16-bit binary representation of %u: ", integer);
 
-    for (int i = 15; i >= 0; i--) {
+    for (int i = (TOTAL_BITS_IN_INT - 1); i >= 0; i--) {
         unsigned int bit = (integer >> i) & 1;
         printf("%u", bit);
         if (i % 4 == 0) {
@@ -29,13 +29,13 @@ int print_binary(uint16_t integer) { // This is a test function for debugging on
 
 uint16_t turn_on_bit_in_array(uint16_t bit_array, unsigned int position) {
     // Check if the function user's given position is out of bounds for safety.
-    if (position > 16) {
+    if (position > TOTAL_BITS_IN_INT) {
         fprintf(stderr, "Error accessing bit array: Given position is out of range.\n");
         return bit_array;
     }
 
     // Shift a bit based on the position given by the function user.
-    uint16_t bit = 0b1000000000000000 >> position;
+    uint16_t bit = SINGULAR_BIT >> position;
 
     // Perform OR on the bit and the existing bit_array to turn on the bit to 1.
     bit_array |= bit;
@@ -45,7 +45,7 @@ uint16_t turn_on_bit_in_array(uint16_t bit_array, unsigned int position) {
 
 bool access_bit_array(uint16_t bit_array, unsigned int position) {
     // Check if the function user's given position is out of bounds for safety.
-    if (position > 16) {
+    if (position > TOTAL_BITS_IN_INT) {
         fprintf(stderr, "Error accessing bit array: Given position is out of range.\n");
         return false;
     }
@@ -57,7 +57,7 @@ bool access_bit_array(uint16_t bit_array, unsigned int position) {
     bit_array >>= true_position;
 
     // Use the rightmost bit to compare 1 with the isolated bit in the bit array using AND.
-    bit_array &= 0b0000000000000001;
+    bit_array &= RIGHTMOST_BIT;
 
     // We want to return a boolean value rather than a 16-bit int because it uses 8 bit (half as
     // much memory). It is also more readable as the resulting JSON array will use a bool.
