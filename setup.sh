6a845bc231c62d106e39d47d28eb250c9ea14f28
Jayden Waghorn <dxmqkgax@duck.com>
2025-07-17 20:28:47 +0100

    build(setup.sh): install `uv` if it is not installed already
    Signed-off-by: Jayden Waghorn <dxmqkgax@duck.com>

--- setup.sh
+++ setup.sh
@@ -1,1 +1,24 @@
 #! /bin/sh
+
+# Install UV locally if it isn't installed already
+if ! command -v uv >/dev/null 2>&1; then
+    echo "uv is not installed. (see https://docs.astral.sh/uv/)"
+    read -p "Do you want to install it? (y/n) " answer
+    if [ "$answer" = "y" ]; then
+        echo "Installing uv"
+	if command -v curl >/dev/null 2>&1; then
+	    echo "Using curl to fetch install script."
+	    curl -LsSf https://astral.sh/uv/install.sh | sh
+	elif command -v wget >/dev/null 2>&1; then
+	    echo "Using wget to fetch install script."
+	    wget -qO- https://astral.sh/uv/install.sh | sh
+	else
+	    exit 1
+	fi
+    else
+	exit 1
+    fi
+else
+    echo "uv is installed."
+fi
+
